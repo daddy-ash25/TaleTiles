ff154ae1b0b8cb12dc12127a19aa2be8899d1401
daddy-ash25 <ashutoshkumar176644@gmail.com>
2025-06-23 14:25:53 +0530

    added a new div containing the game body now

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const sidebar = document.querySelector('.sidebar');
-const menue_button = document.querySelector('.menue-button')
+const menue_button = document.querySelector('.menue-button');
+const mainbody = document.querySelector('.main-body');
+
+menue_button.addEventListener("click",()=>{
+    let sidebar_display_status = getComputedStyle(sidebar).display;
+    if(sidebar_display_status == "none"){
+        sidebar.style.display = "block";
+    }
+    else {
+        sidebar.style.display = "none";
+    }
+
+
+    sidebar_display_status = getComputedStyle(sidebar).display;
+    const screenwidth = window.matchMedia("(min-width: 1025px)");
+    console.log(screenwidth);
+    if(screenwidth.matches == true){
+        if(sidebar_display_status == "none"){
+            mainbody.style.gridColumn = "1/3";
+        }
+        else{
+            mainbody.style.gridColumn = "2/3";
+        }
+    }
+});
 
